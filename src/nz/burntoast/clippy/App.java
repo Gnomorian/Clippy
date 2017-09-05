29ca0775d1942c4654491e87e70daa30d7b6e2fc
William Cameron <gnomorian@gmail.com>
2017-09-05 22:09:26 +1200

    clipboard no longer clips preexisting text, clip cap now implemented
    Clipboard wasnt detecting if you coppied the same text again, it just added it anyway, this has been fixed.
    
    I had a cap of 10 clips before it started deleting old clips, that wasnt working, now it is.

--- src/nz/burntoast/clippy/App.java
+++ src/nz/burntoast/clippy/App.java
@@ -3,7 +3,9 @@ package nz.burntoast.clippy;
 // TODO: on first run, show should i run at startup button
 
 public class App {
+	// controls the Tray icon.
 	public static TrayControl tc;
+	// catches and deals with clipboard events.
 	public static ClipboardControl cc;
 	public static void main(String[] args) {
 	    try {
--- src/nz/burntoast/clippy/ClipboardControl.java
+++ src/nz/burntoast/clippy/ClipboardControl.java
@@ -6,6 +6,8 @@ import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.ClipboardOwner;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class ClipboardControl extends Thread implements ClipboardOwner {
@@ -13,8 +15,6 @@ public class ClipboardControl extends Thread implements ClipboardOwner {
 	int history;
 	// contains the recent strings on the clipboard
 	private ArrayList<MenuItem> stringHistory = new ArrayList<MenuItem>();
-	// contains the recent images on the clipboard
-	private ArrayList<MenuItem> imageHistory = new ArrayList<MenuItem>();
 	
 	Clipboard sysClip = Toolkit.getDefaultToolkit().getSystemClipboard();
 	
@@ -29,7 +29,7 @@ public class ClipboardControl extends Thread implements ClipboardOwner {
 	public void run() {
 	    Transferable trans = sysClip.getContents(this);
 	    regainOwnership(trans);
-	    System.out.println("Listening to board...");
+	    System.out.println("Listening to Clipboard...");
 	    while(true) {}
 	}
 	
@@ -41,14 +41,19 @@ public class ClipboardControl extends Thread implements ClipboardOwner {
 		  sysClip.setContents(transferable, this);
 	  }
 
+	  /**
+	   * get the current object on the Clipboard
+	   * @return
+	   */
 	  public Transferable getClipboardContents() {
 	    Transferable contents = sysClip.getContents(null);
 	    return contents;
 	  }
-	/** use this to find when something new */
+	/** use this to find when something new has been added to the Clipboard */
 	@Override
 	public void lostOwnership(Clipboard board, Transferable clip) {
 		try {
+			// give the other program writing to the Clipboard time to write it.
 		    Thread.sleep(20);
 		  } catch(Exception e) {
 		    System.out.println("Exception: " + e);
@@ -56,24 +61,51 @@ public class ClipboardControl extends Thread implements ClipboardOwner {
 		Transferable contents = sysClip.getContents(this);
 		addNewSavedClip(contents);
 		regainOwnership(contents);
-		System.out.println("clipboard changed, adding new option");
 	}
 	
 	/**
 	 * works out if the contents of clipboard is image or text, then saves it to clipboard
+	 * if the clipboard is over the cap, remove the oldest clip
 	 * @param clipboardContents
 	 */
 	public void addNewSavedClip(Transferable clipboardContents) {
+		// is the clipboard content text?
+		// is it new?
 		boolean hasTransferableText = (clipboardContents != null) && clipboardContents.isDataFlavorSupported(DataFlavor.stringFlavor);
-	    if (hasTransferableText) {
-	      MenuItem clip = App.tc.createTextMenuItem(clipboardContents);
-	      stringHistory.add(clip);
-	      if(stringHistory.size() > 10) {
-	    	  stringHistory.remove(0);
-	      }
+	    if (hasTransferableText && isUnique(clipboardContents, stringHistory)) {
+	    	// check how long the menu list will be
+	    	// add the clipboard content to the Tray
+	    	// if the menu list is longer than the cap, remove old clip
+	    	MenuItem clip = App.tc.createTextMenuItem(clipboardContents);
+	    	stringHistory.add(clip);
+	    	if(stringHistory.size() > 10) {
+		    	  //TODO: add history cap editor to settings menu
+		    	  App.tc.removeMenuItem(stringHistory.remove(0));
+		    	  stringHistory.remove(0);
+		    }
+	    	System.out.println("Clipboard Changed, adding new Item");
 	    }
 	}
 	void regainOwnership(Transferable t) {
 	    sysClip.setContents(t, this);
-	  }
+	}
+	private boolean isUnique(Transferable clipboard, ArrayList<MenuItem> list) {
+		String data = "";
+		try {
+			data = (String)clipboard.getTransferData(DataFlavor.stringFlavor);
+		} catch (UnsupportedFlavorException e) {
+			e.printStackTrace();
+			return false;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+		
+		for(int i = 0; i < list.size(); i++) {
+			if(list.get(i).getLabel().equals(data))
+				return false;
+		}
+		return true;
+		
+	}
 }
